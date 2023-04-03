b5c0c4c1ff2730a0efff2c58aec8ac7a38478ad8
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-04-03 22:21:59 +0200

    Serial simulation for testing

---
+++ temp_sim.R
@@ -0,0 +1,29 @@
+library("readr")
+
+source("performance_analyzer.R")
+# debugSource("performance_analyzer.R")
+
+##############################################################
+# Parameters
+##############################################################
+
+#base_directory <- getwd()
+set.seed(9)
+
+# Read csv with conditions
+conditions <- read_csv(file = 'test_pb_mod_orb_mod_k_15.csv')
+
+# Get 1 condition
+# Each processor gets a chunck of conditions and idx is one row of this chunck
+local_condition = conditions[1,]
+
+# Decompose the row to retrieve the condition's values
+bias_type <- local_condition[[1]]
+delta_00 <- local_condition[[2]]
+num_studies <- local_condition[[3]]
+sigma2_u <- local_condition[[4]]
+sigma2_v <- local_condition[[5]]
+psss <- local_condition[[6]]
+o <- local_condition[[7]]
+
+performance_analyzer(bias_type = bias_type, delta_00 = delta_00,sigma2_u = sigma2_u, sigma2_v = sigma2_v, psss = psss, o = o, k = num_studies, nmeta = 10, verbose = FALSE) # tol = 1e-3, iter = 50,
