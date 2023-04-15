db38117457e2b31ff8c7aecf2aec6ecd648d1705
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-04-15 12:14:41 +0200

    Visualize Rejection rates of test data

---
+++ r_scripts/dataviz_functions.R
@@ -0,0 +1,146 @@
+library(tidyverse)
+# Create dataframe with performance data for all conditions to be visualized
+df_viz <- function(num_studies, delta_00, psss, sigma2_u, sigma2_v, bias_type){
+  df = data.frame()
+
+  for(bt in bias_type){
+    for (k in num_studies) {
+      for(d in delta_00) {
+        for(p in psss) {
+          for(su in sigma2_u) {
+            for(sv in sigma2_v) {
+
+              temp0 <- sprintf("%s", bt)
+              temp1 <- sprintf("k_%d",k)
+              temp2 <- sprintf("d%0.2f_su%0.2f_sv%0.2f_%s", d, su, sv, p)
+              file_path <- file.path("data_from_supercomp","data",temp0, temp1, temp2, "performances.Rdata")
+
+              if(file.exists(file_path)){
+                load(file_path)
+
+                # Create condition ID to the vector
+                id <- sprintf("%s_k_%d_d%0.2f_su%0.2f_sv%0.2f_%s", bt, k, d, su, sv, p)
+
+                # Create ID grouping by psss
+                id_rollup_psss <- sprintf("%s_k_%d_d%0.2f_su%0.2f_sv%0.2f", bt, k, d, su, sv)
+
+                # Add bias_type & condition ID to the vector
+                vec <- cbind(id, id_rollup_psss, bt, as.data.frame(t(performances)))
+
+                # Add performances to dataframe
+                df <- rbind(df,vec)
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  # Add k as character
+  df$k_cat <- as.factor(x = df$k)
+  levels(df$k_cat) <- c("small", "medium", "large")
+  df$k_cat <- as.character(df$k_cat)
+
+  # Add su as character
+  df$sigma2_u_cat <- as.factor(df$sigma2_u)
+  levels(df$sigma2_u_cat) <- c("small", "medium", "large")
+  df$sigma2_u_cat <- as.character(df$sigma2_u_cat)
+
+  # Add sv as character
+  df$sigma2_v_cat <- as.factor(df$sigma2_v)
+  levels(df$sigma2_v_cat) <- c("small", "medium", "large")
+  df$sigma2_v_cat <- as.character(df$sigma2_v_cat)
+
+  # Convert some columns to numeric
+  for (i in 1:dim(df)[2]) {
+    if(!names(df)[i] %in% c("psss","bt", "id", "id_rollup_psss", "k_cat", "sigma2_u_cat", "sigma2_v_cat")) {
+      df[,i] <- as.numeric(df[,i])
+      # df[,i] <- round(df[,i], 4) Round to 4 decimal points
+    }
+  }
+
+  return(df)
+}
+
+# Visualize rejection rates (across k, select only one su = sv)
+viz_rejection_rate <- function(df, num_studies, bias_type, d, su, sv, ss, across){
+
+  # Select from dataframe only the observations of interest
+  df1 <- df %>%
+    filter(k %in% num_studies,
+           bt %in% bias_type,
+           delta_00 %in% d,
+           sigma2_u %in% su,
+           sigma2_v %in% sv,
+           psss %in% ss)
+
+  # Set the correct parameters for the plot
+  if(bias_type == "pb_no_orb_no"){
+    hline = 0.05
+    limits_bounds = c(0, 0.6)
+  } else {
+    hline = 0.80
+    limits_bounds = c(0, 1)
+  }
+
+  if(bias_type == "pb_no_orb_no"){
+    dataset = "No PB, No ORB"
+  } else if(bias_type == "pb_str_orb_no") {
+    dataset = "Strong PB, No ORB"
+  } else if(bias_type == "pb_mod_orb_no") {
+    dataset = "Moderate PB, No ORB"
+  } else if(bias_type == "pb_no_orb_str") {
+    dataset = "No PB, Strong ORB"
+  } else if(bias_type == "pb_no_orb_mod") {
+    dataset = "No PB, Moderate ORB"
+  } else if(bias_type == "pb_str_orb_str") {
+    dataset = "Strong PB, Strong ORB"
+  } else if(bias_type == "pb_mod_orb_mod") {
+    dataset = "Moderate PB, Moderate ORB"
+  } else if (bias_type == "pb_mod_orb_str") {
+    dataset = "Moderate PB, Strong ORB"
+  } else if (bias_type == "pb_str_orb_mod") {
+    dataset = "Strong PB, Moderate ORB"
+  }
+
+  if(across == "k") {
+    # Make plot
+    viz <- df1 %>%
+      ggplot(aes(x = delta_00)) +
+      geom_point(aes(y = rej_pet_slope, color = "SMD"),  pch = 1) +
+      geom_point(aes(y = rej_pet_st_slope, color = "Transformed SMD"), pch = 2) +
+      facet_grid(k_cat ~ fct_rev(psss)) +
+      geom_hline(yintercept = hline, linetype = "dashed") +
+      scale_x_continuous(breaks = c(0, 0.20, 0.50, 0.80), sec.axis = sec_axis(~ . , name = "Primary Studies Sample Size", breaks = NULL, labels = NULL)) +
+      scale_y_continuous(limits = limits_bounds,sec.axis = sec_axis(~ . , name = "Meta-analytic dataset size", breaks = NULL, labels = NULL)) +
+      scale_color_manual(name = "Effect Size", values = c("SMD" = "#E69F00", "Transformed SMD" = "#0072B2")) +
+      labs(y = "Rejection rate",
+           x = "Population SMD",
+           caption = sprintf("Between-study variance: %s. Within-study variance: %s. Bias type: %s.", df1$sigma2_u_cat, df1$sigma2_v_cat, dataset)) +
+      theme_bw()
+  } else if(across == "su_sv"){
+    viz <- df1 %>%
+      ggplot(aes(x = delta_00)) +
+      geom_point(aes(y = rej_pet_slope, color = "SMD"),  pch = 1) +
+      geom_point(aes(y = rej_pet_st_slope, color = "Transformed SMD"), pch = 2) +
+      facet_grid(sigma2_u_cat ~ fct_rev(psss)) +
+      geom_hline(yintercept = hline, linetype = "dashed") +
+      scale_x_continuous(breaks = c(0, 0.20, 0.50, 0.80), sec.axis = sec_axis(~ . , name = "Primary Studies Sample Size", breaks = NULL, labels = NULL)) +
+      scale_y_continuous(limits = limits_bounds,sec.axis = sec_axis(~ . , name = "Between & within study variance", breaks = NULL, labels = NULL)) +
+      scale_color_manual(name = "Effect Size", values = c("SMD" = "#E69F00", "Transformed SMD" = "#0072B2")) +
+      labs(y = "Rejection rate",
+           x = "Population SMD",
+           caption = sprintf("Meta-analytic dataset size: %s. Bias type: %s.", df1$k_cat, dataset)) +
+      theme_bw()
+  }
+
+
+
+  return (viz)
+}
+
+# Visualize rejection rates (across su=sv, select only one k)
+viz_rejection_rage_su_sv <- function(df, num_studies, bias_type, d, su, sv, ss){
+
+}
---
+++ r_scripts/dataviz_test.R
@@ -0,0 +1,112 @@
+debugSource("r_scripts/dataviz_functions.R")
+
+# List all conditions
+k = c(15, 30, 70)
+d = c(0, 0.2, 0.5, 0.8)
+p = c("small", "medium", "large")
+su = c(0.01, 0.06, 0.11)
+sv = c(0.01, 0.06, 0.11)
+bt = c("pb_no_orb_no", "pb_no_orb_str", "pb_str_orb_no", "pb_str_orb_str")
+
+# Create one dataframe with all performance measures across all conditions
+df <- df_viz(num_studies = k, delta_00 = d, psss = p, sigma2_u = su, sigma2_v = sv, bias_type = bt)
+
+# ------------------------------- pb_no_orb_no -----------------------------------
+## Rejection rate: across k, small su=sv  ----------------------------------------
+# Parameters
+num_studies = c(15, 30, 70)
+bias_type = "pb_no_orb_no"
+d = c(0.2)
+su = 0.01
+sv = 0.01
+ss = c("small", "medium", "large")
+# Visualization
+viz_pb_no_orb_no_small_su_sv <- viz_rejection_rate(df = df, num_studies = num_studies, bias_type = bias_type, d = d, su = su, sv = sv, ss = ss, across = "k")
+viz_pb_no_orb_no_small_su_sv
+
+## Rejection rate: across su_sv, small k -------------------------------------
+# Parameters
+num_studies = 15
+bias_type = "pb_no_orb_no"
+d = c(0.2)
+su = c(0.01, 0.06, 0.11)
+sv = c(0.01, 0.06, 0.11)
+ss = c("small", "medium", "large")
+# Visualization
+viz_pb_no_orb_no_small_k <- viz_rejection_rate(df = df, num_studies = num_studies, bias_type = bias_type, d = d, su = su, sv = sv, ss = ss, across = "su_sv")
+viz_pb_no_orb_no_small_k
+
+# Rejection rate: across su_sv, medium k --------------------------------------
+# Parameters
+num_studies = 30
+bias_type = "pb_no_orb_no"
+d = c(0.2)
+su = c(0.01, 0.06, 0.11)
+sv = c(0.01, 0.06, 0.11)
+ss = c("small", "medium", "large")
+# Visualization
+viz_pb_no_orb_no_medium_k <- viz_rejection_rate(df = df, num_studies = num_studies, bias_type = bias_type, d = d, su = su, sv = sv, ss = ss, across = "su_sv")
+viz_pb_no_orb_no_medium_k
+
+# Rejection rate: across su_sv, large k ----------------------------------------
+# Parameters
+num_studies = 70
+bias_type = "pb_no_orb_no"
+d = c(0.2)
+su = c(0.01, 0.06, 0.11)
+sv = c(0.01, 0.06, 0.11)
+ss = c("small", "medium", "large")
+# Visualization
+viz_pb_no_orb_no_large_k <- viz_rejection_rate(df = df, num_studies = num_studies, bias_type = bias_type, d = d, su = su, sv = sv, ss = ss, across = "su_sv")
+viz_pb_no_orb_no_large_k
+
+
+# ------------------------------- pb_no_orb_str ---------------------------------------------
+## Rejection rate: across k, small su=sv ---------------
+# Parameters
+num_studies = c(15, 30, 70)
+bias_type = "pb_no_orb_str"
+d = c(0.2)
+su = 0.01
+sv = 0.01
+ss = c("small", "medium", "large")
+# Visualization
+viz_pb_no_orb_str_small_su_sv <- viz_rejection_rate(df = df, num_studies = num_studies, bias_type = bias_type, d = d, su = su, sv = sv, ss = ss, across = "k")
+viz_pb_no_orb_str_small_su_sv
+
+## Rejection rate: across su_sv, small k -------------------------------------
+# Parameters
+num_studies = 15
+bias_type = "pb_no_orb_str"
+d = c(0.2)
+su = c(0.01, 0.06, 0.11)
+sv = c(0.01, 0.06, 0.11)
+ss = c("small", "medium", "large")
+# Visualization
+viz_pb_no_orb_str_small_k <- viz_rejection_rate(df = df, num_studies = num_studies, bias_type = bias_type, d = d, su = su, sv = sv, ss = ss, across = "su_sv")
+viz_pb_no_orb_str_small_k
+
+# Rejection rate: across su_sv, medium k --------------------------------------
+# Parameters
+num_studies = 30
+bias_type = "pb_no_orb_str"
+d = c(0.2)
+su = c(0.01, 0.06, 0.11)
+sv = c(0.01, 0.06, 0.11)
+ss = c("small", "medium", "large")
+# Visualization
+viz_pb_no_orb_str_medium_k <- viz_rejection_rate(df = df, num_studies = num_studies, bias_type = bias_type, d = d, su = su, sv = sv, ss = ss, across = "su_sv")
+viz_pb_no_orb_str_medium_k
+
+# Rejection rate: across su_sv, large k ----------------------------------------
+# Parameters
+num_studies = 70
+bias_type = "pb_no_orb_str"
+d = c(0.2)
+su = c(0.01, 0.06, 0.11)
+sv = c(0.01, 0.06, 0.11)
+ss = c("small", "medium", "large")
+# Visualization
+viz_pb_no_orb_str_large_k <- viz_rejection_rate(df = df, num_studies = num_studies, bias_type = bias_type, d = d, su = su, sv = sv, ss = ss, across = "su_sv")
+viz_pb_no_orb_str_large_k
+
