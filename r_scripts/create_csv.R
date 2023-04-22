e572d48d04d0780953d1c43c33f971f5a676f75b
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-04-22 20:11:13 +0200

    Conditions for descriptives added

--- r_scripts/create_csv.R
+++ r_scripts/create_csv.R
@@ -44,13 +44,10 @@ conditions_csv <- function(bias_type, k){
 
 }
 
-
 # UNBIASED
 num_studies <- c(15, 30, 70)
-for (k in num_studies){
-  testing_conditions_csv(bias_type = "pb_no_orb_no", k = k)
-  conditions_csv(bias_type = "pb_no_orb_no", k = k)
-}
+conditions_csv(bias_type = bias_type, k = num_studies)
+
 ## Check
 ## test <- read.csv("conditions/pb_no_orb_no_k_30.csv")
 
@@ -110,3 +107,35 @@ for (k in num_studies){
   testing_conditions_csv(bias_type = "pb_str_orb_mod", k = k)
   conditions_csv(bias_type = "pb_str_orb_mod", k = k)
 }
+
+# FULL BIASED
+# Function ----
+biased_conditions_csv <- function(bias_type){
+  bias_type <- bias_type
+  delta_00 <- c(0, 0.2, 0.5, 0.8)
+  num_studies <- c(15, 30, 70)
+  sigma2u <- c(0.01, 0.06, 0.11)
+  sigma2v <- c(0.01, 0.06, 0.11)
+  psss <- c("small", "medium", "large")
+  o <- 3
+
+  # Combine condition factors into data frame
+  conditions <- expand.grid(bias_type, delta_00, num_studies, sigma2u, sigma2v, psss, o)
+  names(conditions) <- c("bias_type", "delta_00", "num_studies", "sigma2u", "sigma2v", "psss", "o")
+
+  # Save as csv
+  name <- sprintf("condition_%s.csv", bias_type)
+  write.csv(conditions, name, row.names = FALSE)
+}
+# Generate csvs ----
+biased_conditions_csv("pb_no_orb_str")
+biased_conditions_csv("pb_no_orb_mod")
+biased_conditions_csv("pb_str_orb_no")
+biased_conditions_csv("pb_mod_orb_no")
+biased_conditions_csv("pb_str_orb_str")
+biased_conditions_csv("pb_mod_orb_mod")
+biased_conditions_csv("pb_str_orb_mod")
+biased_conditions_csv("pb_mod_orb_str")
+
+
+
