422c621a2c7bd8e59fe8e8aabd32c6a143b48f0e
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-04-27 11:33:26 +0200

    Add tabPanel "Type I error - M.Egger"

--- final_app.R
+++ final_app.R
@@ -26,6 +26,8 @@ ui <- fluidPage(
                               plotOutput("viz_hist_perc_excluded_by_d"),
                               plotOutput("hist_perc_out_excluded_by_psss")
                             ))
+    ),
+    tabPanel("Type I error - M.Egger"
     )
   )
 )
