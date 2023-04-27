758baab4df67aaa5e2ea9387ef1769b3eb0ffa78
Giulia Bertoldo <giulia.bertoldo.gb@gmail.com>
2023-04-27 11:28:50 +0200

    table_perc_out_excluded_by_d

--- final_app.R
+++ final_app.R
@@ -21,7 +21,8 @@ ui <- fluidPage(
 
                               "Percentage of outcomes excluded from the original dataset",
                               tableOutput("table_perc_out_excl"),
-                              plotOutput("hist_perc_out_excl")
+                              plotOutput("hist_perc_out_excl"),
+                              tableOutput("table_perc_out_excluded_by_d")
                             ))
     )
   )
@@ -39,6 +40,10 @@ server <- function(input, output, session){
 
   output$hist_perc_out_excl <- renderPlot({
     viz_hist_perc_excluded(df = df)
+  })
+
+  output$table_perc_out_excluded_by_d <- renderTable({
+    table_perc_out_excluded_by_bt_delta(df = df)
   })
 }
 
