dbe144317f10f380e28331783ca84a4c0ae751c2
vsc35419 <vsc35419@tier2-p-login-3.genius.hpc.kuleuven.be>
2023-04-28 10:13:23 +0200

    script for pet_results folder

--- r_scripts/convergence_check.R
+++ r_scripts/convergence_check.R
@@ -1,5 +1,15 @@
 # Convert pets_results to dataframe
 
+# Z score 
+z_score <- function(df, col){
+  mean_col <- mean(df[, col], na.rm = T)
+  sd <- sd(df[, col], na.rm = T)
+  z_col_name <- sprintf("z_%s", col)
+  df[, z_col_name] <- NA 
+  df[, z_col_name] <- (df[, col] - mean_col) / sd
+  return(df)
+}
+
 # Function
 save_pets_results <- function(bt, k, d, su, sv, p){
   temp0 <- sprintf("%s", bt)
@@ -12,19 +22,54 @@ save_pets_results <- function(bt, k, d, su, sv, p){
   # Convert to dataframe
   df <- as.data.frame(do.call(rbind, pets_results))
   
-  # Save in 
-  write.csv(df, file.path("pet_results", sprintf("%s_k%g_d%g_su%g_sv%g_%s.csv", bt, k, d, su, sv, p)), row.names = FALSE)
+  # Add columns with z scores 
+  df <- z_score(df, "pet_int")
+  df <- z_score(df, "pet_st_int")
+  df <- z_score(df, "pet_slope")
+  df <- z_score(df, "pet_st_slope")
+ 
+  df <- z_score(df, "peese_int")
+  df <- z_score(df, "peese_st_int")
+  df <- z_score(df, "peese_slope")
+  df <- z_score(df, "peese_st_slope")
+  
+  df <- z_score(df, "corrected_smd")
+  df <- z_score(df, "corrected_st_smd")
+  
+  # Save in: 
+  save_path_bt <- file.path("pet_results", sprintf("%s", bt))
+  if (!file.exists(save_path_bt)){
+    dir.create(save_path_bt)
+  }
+  save_path_k <- file.path("pet_results", sprintf("%s", bt), sprintf("k_%g", k))
+  if (!file.exists(save_path_k)){
+    dir.create(save_path_k)
+  }
+  write.csv(df, file.path(save_path_bt, sprintf("k_%g", k), sprintf("pet_results_d%g_su%g_sv%g_%s.csv", d, su, sv, p)), row.names = FALSE)
 }
 
-# Use function
-save_pets_results(bt = "pb_str_orb_no", k = 15, d = 0, su = 0.01, sv = 0.01, p = "small")
-save_pets_results(bt = "pb_str_orb_no", k = 15, d = 0, su = 0.01, sv = 0.01, p = "medium")
-save_pets_results(bt = "pb_str_orb_no", k = 15, d = 0, su = 0.01, sv = 0.01, p = "large")
+bias_type = c("pb_no_orb_no", "pb_no_orb_str", "pb_no_orb_mod", "pb_str_orb_no", "pb_mod_orb_no")
+num_studies = c(15, 30, 70)
+delta_00 = c(0, 0.2, 0.5, 0.8)
+sigma2_u = c(0.01, 0.06, 0.11)
+sigma2_v =c(0.01, 0.06, 0.11)
+psss = c("small", "medium", "large")
 
-save_pets_results(bt = "pb_str_orb_no", k = 15, d = 0, su = 0.06, sv = 0.06, p = "small")
-save_pets_results(bt = "pb_str_orb_no", k = 15, d = 0, su = 0.06, sv = 0.06, p = "medium")
-save_pets_results(bt = "pb_str_orb_no", k = 15, d = 0, su = 0.06, sv = 0.06, p = "large")
+start.time <- Sys.time()
 
-save_pets_results(bt = "pb_str_orb_no", k = 30, d = 0, su = 0.01, sv = 0.01, p = "small")
+for (bt in bias_type){
+  for(k in num_studies){
+    for(d in delta_00){
+      for(su in sigma2_u){
+        for(sv in sigma2_v){
+          for(p in psss){
+            save_pets_results(bt = bt, k = k, d = d, su = su, sv = sv, p = p)
+          }
+        }
+      }
+    }
+  }
+}
 
-save_pets_results(bt = "pb_mod_orb_no", k = 15, d = 0, su = 0.01, sv = 0.01, p = "small")
+end.time <- Sys.time()
+(time.taken <- end.time - start.time)
